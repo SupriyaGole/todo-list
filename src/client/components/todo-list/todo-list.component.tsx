2ee66fa0cb765d4b2ecb3341b3de73091a8102e8
Supriya Gole <supriya.anandrao.gole@zalando.de>
2023-02-23 12:29:05 +0100

    feat: Style todo list

--- src/client/components/todo-list/todo-list.component.tsx
+++ src/client/components/todo-list/todo-list.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { TodoListReducer } from '../../types/todos.types';
 import TodoCreator from "../todo-creator/todo-creator.component";
+import './todo-list.css';
 
 const TodoList = () => {
   const {todos} = useSelector(({todoList}: TodoListReducer) => ({
@@ -9,10 +10,10 @@ const TodoList = () => {
   }));
 
   return (
-    <div>
+    <div className='main-page'>
       <TodoCreator />
       <div className='list-of-todos'>
-        {todos.map((item) => <p>{item.description}</p>)}
+        {todos.map((item) => <p className='todo'>{item.description}</p>)}
       </div>
     </div>
   );
--- src/client/components/todos.component.tsx
+++ src/client/components/todos.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodoHeader from './todo-header/todo-header.component';
 import TodoList from "./todo-list/todo-list.component";
+import './todos.css';
 
 const Todos = () => {
   return (
